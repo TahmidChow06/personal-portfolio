31063b691d6a44fe42e9d139fa17b340be33e011
TahmidChow <>
2021-04-29 10:51:58 +0000

    main.js

--- js/main.js
+++ js/main.js
@@ -352,11 +352,6 @@ $(function(){
               },
               "polygon": {
                 "nb_sides": 5
-              },
-              "image": {
-                "src": "img/github.svg",
-                "width": 100,
-                "height": 100
               }
             },
             "opacity": {
